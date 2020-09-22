13ccc1bcd77f2b80c79ea95835eb9d805e96d1b8
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2020-09-22 19:31:17 +0800

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r', encoding='utf8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="graiax-nem",
-    version="0.0.3.dev1",
-    author="Xiao_Jin",
+    name='graiax-nem',
+    version='0.0.3.dev1',
+    author='Xiao_Jin',
     keywords='graia graiax nem graiax-nem graia-message',
-    description="适用于 Graia Framework 的消息解析过滤器",
+    description='适用于 Graia Framework 的消息解析过滤器',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jinzhijie/graiax-nem",
+    long_description_content_type='text/markdown',
+    url='https://github.com/jinzhijie/graiax-nem',
     packages=['graiax/nem'],
     install_requires=['graia-application-mirai'],
     python_requires='>=3.7',

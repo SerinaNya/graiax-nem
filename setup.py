0e03cc9533637350e36c98c77a9a60043fa166d2
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2020-09-22 19:38:08 +0800

    Release 0.0.3.dev2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf8') as fh:
 
 setuptools.setup(
     name='graiax-nem',
-    version='0.0.3.dev1',
+    version='0.0.3.dev2',
     author='Xiao_Jin',
     keywords='graia graiax nem graiax-nem graia-message',
     description='适用于 Graia Framework 的消息解析过滤器',
